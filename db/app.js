ce74bb02a343e462ba9dcb9dcbf35e48c65df73e
Saad Qureshi <saad-qureshi2001@outlook.com>
2023-10-03 11:39:56 +0100

    Task 4 get-article-id completed

--- db/app.js
+++ db/app.js
@@ -1,22 +1,27 @@
 const express = require("express");
-const {getAllTopics, getAllEndpoints} = require("./controller");
+const {
+	getAllTopics,
+	getAllEndpoints,
+	getArticleById,
+} = require("./controller");
 const app = express();
 
 app.get("/api/topics", getAllTopics);
-app.get("/api", getAllEndpoints)
-
+app.get("/api", getAllEndpoints);
+app.get("/api/articles/:article_id", getArticleById);
 
 // Error Handling
 app.use((err, req, res, next) => {
-    console.log(err)
+	console.log(err);
 	if (err.status) {
 		res.status(err.status).send({ msg: err.msg });
+	} else {
+		res.status(500).send({ msg: "Internal Server Error" }); // Default error response
 	}
 });
 
-
-app.all('*', (req, res) => {
-    res.status(404).send({msg: 'Invalid Path'})
-})
+app.all("*", (req, res) => {
+	res.status(404).send({ msg: "Invalid Path" });
+});
 
 module.exports = app;
--- db/model.js
+++ db/model.js
@@ -1,12 +1,35 @@
-const db = require('./connection')
+const db = require("./connection");
 
 exports.selectAllTopics = () => {
-    let query = `SELECT * FROM topics`;
+	let query = `SELECT * FROM topics`;
 
-    return db.query(query).then((topics) => {
+	return db.query(query).then((topics) => {
 		if (!topics.rows.length) {
 			return Promise.reject({ status: 404, msg: "Not Found" });
 		}
 		return topics.rows;
+	});
+};
+
+exports.selectArticleById = (articleId) => {
+	const query = `
+	  SELECT 
+		author, 
+		title, 
+		article_id, 
+		body, 
+		topic, 
+		created_at, 
+		votes, 
+		article_img_url
+	  FROM articles
+	  WHERE article_id = $1`;
+
+	return db.query(query, [articleId]).then((result) => {
+		if (result.rows.length === 0) {
+			return Promise.reject({ status: 404, msg: "Article not found" });
+		}
+
+		return result.rows[0];
 	});
 };
