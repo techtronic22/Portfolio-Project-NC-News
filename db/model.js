421bf19a2279ad903fe281b7dbb65898df41d6e9
Saad Qureshi <saad-qureshi2001@outlook.com>
2023-10-06 14:03:36 +0100

    Task 11 attempted

--- db/model.js
+++ db/model.js
@@ -38,27 +38,61 @@ exports.selectArticleById = (articleId) => {
 	});
 };
 
-exports.selectAllArticles = (sort_by = "created_at", order = "desc") => {
-	let query = `
-    SELECT 
-        articles.article_id,
-        articles.author,
-        articles.title,
-        articles.topic,
-        articles.created_at,
-        articles.votes,
-        articles.article_img_url,
-		COUNT(comments.article_id) AS comment_count
-    FROM articles LEFT JOIN comments ON articles.article_id = comments.article_id 
-	GROUP BY articles.article_id
-	ORDER BY articles.created_at DESC;`;
+exports.selectAllArticles = (sort_by, order, topic) => {
+	const validSorts = [
+		"author_id",
+		"title",
+		"topic",
+		"author",
+		"created_at",
+		"votes",
+	];
 
-	return db.query(query).then((articles) => {
-		if (!articles.rows.length) {
-			return Promise.reject({ status: 404, msg: "Not Found" });
-		}
-		return articles.rows;
-	});
+	let query = `SELECT * FROM articles `;
+
+	if (typeof topic === "string" && !isNaN(Number(topic))) {
+		return Promise.reject({ status: 400, msg: "Bad Request" });
+	} else if (topic !== undefined) {
+		const queryTopic = `WHERE topic = $1`;
+		return db.query(query + queryTopic, [topic]).then((articles) => {
+			if (!articles.rows.length)
+				return Promise.reject({ status: 404, msg: "Not Found" });
+			return articles.rows;
+		});
+	}
+
+	if (sort_by && !validSorts.includes(sort_by)) {
+		return Promise.reject({ status: 400, msg: "Bad Request" });
+	}
+
+	if (order && order !== "asc" && order !== "desc") {
+		return Promise.reject({ status: 400, msg: "Bad Request" });
+	} else if (validSorts.includes(sort_by)) {
+		const querySortBy = `ORDER BY ${sort_by}`;
+		return db.query(query + querySortBy).then((articles) => {
+			return articles.rows;
+		});
+	} else if (order) {
+		const queryOrderBy = `ORDER BY $1`;
+		return db.query(query + queryOrderBy, [order]).then((articles) => {
+			return articles.rows;
+		});
+	} else {
+		return db
+			.query(
+				`SELECT articles.article_id, articles.title, articles.topic, articles.author, articles.created_at, articles.votes, articles.article_img_url, COUNT(comments.article_id) AS comment_count
+        FROM articles LEFT JOIN comments ON articles.article_id = comments.article_id
+        GROUP BY articles.article_id
+        ORDER BY articles.created_at DESC;`
+			)
+			.then((articles) => {
+				let filteredArticles = articles.rows;
+				filteredArticles = filteredArticles.map((article) => {
+					return article;
+				});
+				return filteredArticles;
+			});
+	}
 };
 
 exports.selectCommentsById = (articleId) => {
@@ -84,78 +118,79 @@ exports.selectCommentsById = (articleId) => {
 };
 
 exports.insertComment = (newComment, article_id) => {
+	const { username, body } = newComment;
 
-    const { username, body } = newComment;
+	return db
+		.query(`SELECT * FROM articles WHERE article_id = $1`, [article_id])
+		.then((articles) => {
+			if (articles.rows.length === 0) {
+				return Promise.reject({ status: 404, msg: "Not Found" });
+			}
 
+			if (body === "") {
+				return Promise.reject({ status: 400, msg: "Bad Request" });
+			}
 
-    return db
-        .query(`SELECT * FROM articles WHERE article_id = $1`, [article_id])
-        .then((articles) => {
-            if (articles.rows.length === 0) {
-                return Promise.reject({ status: 404, msg: "Not Found" });
-            }
-
-            if (body === "") {
-                return Promise.reject({ status: 400, msg: "Bad Request" });
-            }
-
-            return db.query(
-                `INSERT INTO comments (body, article_id, author) 
+			return db.query(
+				`INSERT INTO comments (body, article_id, author) 
                 VALUES ($1, $2, $3) 
-                RETURNING *;`, [body, article_id, username]
-            );
-        })
-        .then((data) => {
-            return data.rows[0]
-        });
+                RETURNING *;`,
+				[body, article_id, username]
+			);
+		})
+		.then((data) => {
+			return data.rows[0];
+		});
 };
 
 exports.updateArticleVote = (article_id, inc_votes) => {
+	if (isNaN(Number(inc_votes))) {
+		return Promise.reject({ status: 400, msg: "Bad Request" });
+	}
+	if (inc_votes > 0) {
+		return db
+			.query(
+				`UPDATE articles SET votes = votes + $1 WHERE article_id = $2 RETURNING *`,
+				[inc_votes, article_id]
+			)
+			.then((res) => {
+				if (!res.rows.length) {
+					return Promise.reject({ status: 404, msg: "Not Found" });
+				}
 
-    if (isNaN(Number(inc_votes))) {
-        return Promise.reject({ status: 400, msg: "Bad Request" });
-    }
-    if (inc_votes > 0) {
-        return db.query(
-            `UPDATE articles SET votes = votes + $1 WHERE article_id = $2 RETURNING *`, [inc_votes, article_id]
-        )
-        .then((res) => {
-            if (!res.rows.length) {
-                return Promise.reject({ status: 404, msg: "Not Found" });
-            }
+				return res.rows[0];
+			});
+	} else {
+		return db
+			.query(
+				`UPDATE articles SET votes = votes + $1 WHERE article_id = $2 RETURNING *`,
+				[inc_votes, article_id]
+			)
+			.then((res) => {
+				if (!res.rows.length) {
+					return Promise.reject({ status: 404, msg: "Not Found" });
+				}
 
-            return res.rows[0];
-        });
-    }
-	
-	else {
-		return db.query(
-            `UPDATE articles SET votes = votes + $1 WHERE article_id = $2 RETURNING *`, [inc_votes, article_id]
-        ).then((res) => {
-            if (!res.rows.length) {
-                return Promise.reject({ status: 404, msg: "Not Found" });
-            }
-
-            return res.rows[0];
-        });
+				return res.rows[0];
+			});
 	}
 };
 
 exports.deleteComment = (comment_id) => {
 	return db
-	.query(`SELECT * FROM comments WHERE comment_id = $1`, [comment_id])
-	.then((comments) => {
-		if (!comments.rows.length) {
-			return Promise.reject({ status: 404, msg: "Not Found" });
-		}
-		return db
-		.query(`DELETE FROM comments WHERE comment_id = $1 `, [comment_id])
-	})
-}
+		.query(`SELECT * FROM comments WHERE comment_id = $1`, [comment_id])
+		.then((comments) => {
+			if (!comments.rows.length) {
+				return Promise.reject({ status: 404, msg: "Not Found" });
+			}
+			return db.query(`DELETE FROM comments WHERE comment_id = $1 `, [
+				comment_id,
+			]);
+		});
+};
 
 exports.selectAllUsers = () => {
-	return db.query(`SELECT * FROM users`)
-	.then((usersData) => {
-		return usersData.rows
-	})
-}
+	return db.query(`SELECT * FROM users`).then((usersData) => {
+		return usersData.rows;
+	});
+};
