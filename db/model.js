45f19335449c0bfec5b9ef67f4702e58c81a8a37
Saad Qureshi <saad-qureshi2001@outlook.com>
2023-10-06 09:50:40 +0100

    Task 9 Updated

--- db/model.js
+++ db/model.js
@@ -151,7 +151,5 @@ exports.deleteComment = (comment_id) => {
 		return db
 		.query(`DELETE FROM comments WHERE comment_id = $1 `, [comment_id])
 	})
-	.then((response) => {
-		return response.rows
-	})
+	
 }
